cbba1dcbc9ebdce45fe66d549d969dee9da81203
seandrod <sergio.rod.perez@outlook.es>
2023-03-08 15:47:59 -0500

    Agregado - AuthCorsParaTodos

--- src/main/java/com/paquetecontable/main/bussines/UserBussines.java
+++ src/main/java/com/paquetecontable/main/bussines/UserBussines.java
@@ -1,13 +1,10 @@
 package com.paquetecontable.main.bussines;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import com.paquetecontable.main.Models.Client.PUC.PUCClientModel;
 import com.paquetecontable.main.Models.User.User;
 import com.paquetecontable.main.repo.CRUDUsers;
 
